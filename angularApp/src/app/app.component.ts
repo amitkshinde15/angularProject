b1ed722279573561755f7922cecfcf19356fc5d1
amitkshinde15 <amitkshinde15@gmail.com>
2023-02-03 18:49:54 +0530

    Changes done
    Changes done

--- angularApp/src/app/app.component.ts
+++ angularApp/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  test = 'Used property binding using div tag';
   title = 'angularApp';
+  name = 'Amit';
+  empCount;
+  date;
+  
+  data = {
+    name : 'Codeminde',
+    dept : 'Technology'
+  }
+  constructor(){
+    this.getValueFromServer();
+    this.getCurrentDate();
+  
+  }
+  getValueFromServer(){
+    this.empCount = 100000;
+    
+    
+  }
+  imageUrl = 'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg';
+  getCurrentDate(){
+    this.date = Date();
+  }
+
 }
+
--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    Test1Component,
+    Test2Component
   ],
   imports: [
     BrowserModule,
